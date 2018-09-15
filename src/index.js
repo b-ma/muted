ebd218eb8f32d9fa1da93b840ab6c5d076818f18
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2018-09-15 14:24:37 +0200

    cleaned README

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
+import Scene from './lib/areas/Scene';
 
-class MyClass {
-  constructor() {
-    console.log(this);
-  }
+const scene = new Scene('my-id');
+scene.render();
+scene.editable = true;
 
-  * generatorMethod() {
-    let i = 0;
+// create component
+const elms = [];
 
-    while (i < 10)
-      yield i++;
-
-    return 'done';
-  }
+for (let i = 0; i < 10; i++) {
+  const a = scene.createComponent('number-box', 64 * i + 100, 50);
+  elms.push(a);
 }
 
-const myClass = new MyClass();
-console.log([...myClass.generatorMethod()]);
+const target = `maintenant`;
+let index = 0;
+
+(function advance() {
+  elms.forEach(elm => elm.value = Math.random());
+
+  setTimeout(advance, 20);
+})();
+// a.outputChannel = 'my-component'; // should come from inspector config
+
+// scene.createInspector(a);
+
+// listeners
+// scene.addListener('my-number', value => console.log('number', value));
+
+window.scene = scene;
+
+// delete
+// setTimeout(() => scene.deleteComponent(a), 6000);
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+// for (let i = 0; i < 10000; i++) {
+//   const x = Math.floor(Math.random() * width - 10);
+//   const y = Math.floor(Math.random() * height - 10);
+//   scene.createComponent('toggle', x, y);
+// }
+
+
+scene.addListener('test', value => console.log('toggle', value));
+
+const synth = window.speechSynthesis;
+const utterThis = new SpeechSynthesisUtterance(target);
+synth.speak(utterThis);
